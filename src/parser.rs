724dc194520d886efaa6c5634f854c70a3c4399e
Sören Reichardt <soren.reichardt@neotechnology.com>
2022-12-14 17:02:28 +0100

    Implement group parsing

--- src/parser.rs
+++ src/parser.rs
@@ -148,36 +148,57 @@ impl<'a> Parser<'a> {
     fn primary(&mut self) -> Result<Expression> {
         let expression = match self.token_iterator.peek() {
             Some(token) => match token {
-                Token { token_type: TokenType::False, .. } => Some(Expression::Literal(LiteralValue::Boolean(false))),
-                Token { token_type: TokenType::True, .. } => Some(Expression::Literal(LiteralValue::Boolean(true))),
-                Token { token_type: TokenType::Nil, .. } => Some(Expression::Literal(LiteralValue::Nil)),
-                Token { token_type: TokenType::Number, lexeme, .. } => Some(Expression::Literal(LiteralValue::Number(lexeme.parse::<f64>().unwrap()))),
-                Token { token_type: TokenType::String, lexeme, .. } => Some(Expression::Literal(LiteralValue::String(lexeme.to_string()))),
-                _ => None
+                Token { token_type: TokenType::False, .. } => Ok(Expression::Literal(LiteralValue::Boolean(false))),
+                Token { token_type: TokenType::True, .. } => Ok(Expression::Literal(LiteralValue::Boolean(true))),
+                Token { token_type: TokenType::Nil, .. } => Ok(Expression::Literal(LiteralValue::Nil)),
+                Token { token_type: TokenType::Number, lexeme, .. } => Ok(Expression::Literal(LiteralValue::Number(lexeme.parse::<f64>().unwrap()))),
+                Token { token_type: TokenType::String, lexeme, .. } => Ok(Expression::Literal(LiteralValue::String(Self::remove_first_and_last(lexeme).to_string()))),
+                Token { token_type: TokenType::LeftParen, .. } => {
+                    self.token_iterator.next();
+                    let expression = self.expression()?;
+                    self.consume(&TokenType::RightParen)?;
+                    Ok(Expression::Grouping(Box::new(expression)))
+                },
+                _ => Err(Error::ParserError("Could not match expression".to_string()))
             },
-            None => None
-        };
+            None => Err(Error::ParserError("Reached end of file while parsing".to_string()))
+        }?;
         
-        match expression {
-            Some(expression) => {
+        self.token_iterator.next();
+        Ok(expression)
+    }
+
+    fn consume(&mut self, expected_token_type: &TokenType) -> Result<()> {
+        match self.token_iterator.peek().map(|token| token.token_type) {
+            Some(token_type) if &token_type == expected_token_type => {
                 self.token_iterator.next();
-                return Ok(expression);
+                Ok(())
             },
-            None => Err(Error::ParserError("Could not match expression".to_string()))
+            Some(_) | None => Err(Error::ParserError(format!("Expected token to be of type {:?}", expected_token_type)))
         }
+    }
+
+    fn remove_first_and_last(value: &str) -> &str {
+        let mut chars = value.chars();
+        chars.next();
+        chars.next_back();
+        chars.as_str()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{scanner::Scanner, context::Context};
+    use crate::context::Context;
 
     use super::*;
 
     #[test]
-    fn should_parse_expression() {
-        let mut scanner = Scanner::new("1 == 2", Context::new());
-        let tokens = scanner.scan_tokens().unwrap();
+    fn should_parse_comparison_expression() {
+        let tokens = vec![
+            Token::new(TokenType::Number, "1", 1),
+            Token::new(TokenType::EqualEqual, "==", 1),
+            Token::new(TokenType::Number, "2", 1)
+        ];
         let mut parser = Parser::new(tokens, Context::new());
         let expression = parser.expression().unwrap();
         assert_eq!(
@@ -188,5 +209,22 @@ mod tests {
             ),
             expression
         );
+    }
+
+    #[test]
+    fn should_parse_grouping_expression() {
+        let tokens = vec![
+            Token::new(TokenType::LeftParen, "(", 1),
+            Token::new(TokenType::String, "\"foo\"", 1),
+            Token::new(TokenType::RightParen, ")", 1),
+        ];
+        let mut parser = Parser::new(tokens, Context::new());
+        let expression = parser.expression().unwrap();
+        assert_eq!(
+            Expression::Grouping(
+                Box::new(Expression::Literal(LiteralValue::String("foo".to_string())))
+            ),
+            expression
+        )
     }
 }
